b51339ae2bdd12d49131a10eff322db66e1bd25f
Kpearson72 <kpearson72@gmail.com>
2021-01-15 21:06:19 -0600

    added completed png of ERD and Quick DBD-ERD1-HW with data analysis

---
+++ EmployeeSQL/SQL_files/QuickDBD-ERD1-HW (2).sql
@@ -0,0 +1,67 @@
+﻿-- drop tables if exists
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Create tables departments, titles, employees, dept_emp, dept_manager, and salaries
+CREATE TABLE departments (
+    dept_no VARCHAR PRIMARY KEY NOT NULL,
+    dept_name VARCHAR NOT NULL
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR PRIMARY KEY  NOT NULL,
+    title VARCHAR NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY NOT NULL,
+    emp_title_id VARCHAR  NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT  NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    dept_no VARCHAR   NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no,dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    salary INT   NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+	JOIN salaries AS s
+	ON (s.emp_no = e.emp_no);
+	
+-- List first name, last name, and hire date for employees who were hired in 1986.
+	
+SELECT  first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '12/31/1985' AND '1/1/1987';
+
+
+
---
+++ EmployeeSQL/SQL_files/QuickDBD-ERD1-HW.sql
@@ -0,0 +1,77 @@
+﻿
+
+CREATE TABLE departments (
+    dept_no INT  PRIMARY KEY NOT NULL ,
+    dept_name VARCHAR  NOT NULL 
+);
+____________________________________
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY NOT NULL ,
+    emp_title_id INT  NOT NULL ,
+    birth_date DATE  NOT NULL ,
+    first_name VARCHAR  NOT NULL ,
+    last_name VARCHAR  NOT NULL ,
+    sex VARCHAR  NOT NULL ,
+    hire_date DATE  NOT NULL 
+);
+____________________________________
+
+CREATE TABLE dept_emp (
+    emp_no INT  NOT NULL ,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    dept_no INT  NOT NULL, 
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+____________________________________
+
+CREATE TABLE dept_manager (
+    dept_no INT  NOT NULL ,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    emp_no INT  NOT NULL, 
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+____________________________________
+CREATE TABLE salary (
+    emp_no INT  NOT NULL ,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    salary INT  NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+____________________________________
+CREATE TABLE title (
+    title_id VARCHAR  NOT NULL ,
+    title VARCHAR  NOT NULL 
+)
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_emp_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [departments] ([dept_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [departments] ([dept_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_emp_no]
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_title_id] FOREIGN KEY([emp_title_id])
+REFERENCES [title] ([title_id])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_title_id]
+
+ALTER TABLE [salary] WITH CHECK ADD CONSTRAINT [FK_salary_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [salary] CHECK CONSTRAINT [FK_salary_emp_no]
+
